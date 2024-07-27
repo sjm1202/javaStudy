d4517b2b65552c9bc9f459a8cee803a616a74dcd
shenjinmu <shenjinmu1202@gmail.com>
2024-07-27 21:19:37 +0800

    0727

---
+++ workspace/basic-code/day02-code/src/com/itmeima/demo1/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.itmeima.demo1;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 注释
+        System.out.println("hello world");
+
+        //
+        String a = "aaa";
+    }
+}
